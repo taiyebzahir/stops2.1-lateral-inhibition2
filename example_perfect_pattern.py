8934ae7798b1da8b272d252f49bb599169efa991
taiyeb zahir <taiyebzahir13@gmail.com>
2014-07-24 18:28:08 +0200

    Update example_perfect_pattern.py

--- example_perfect_pattern.py
+++ example_perfect_pattern.py
@@ -6,22 +6,21 @@ import numpy
 from utils import generate_pop, HexGrid, draw_hex_grid
 from stops_ import Stops2
 
-secretion = numpy.array([5, 6])
-reception = numpy.array([3, 4])
+secretion = numpy.array([4, 5])
+reception = numpy.array([2, 3])
 receptors = numpy.array([-1, -1])
-bound=numpy.array([1,1,1,1,1,1,1])
+bound=numpy.array([1,1,1,1,1,1])
 
-base1=numpy.array([0,0,1,0,0,0,0])
+base1=numpy.array([0,1,0,0,0,0])
 
 
-trans_mat = numpy.array([[0,-10,0,0,0,0,10], #notch
-                         [0,0,0,0,0,1,0], #Delta
-                         [0,1,0,0,0,0,0], #basal
-                         [0.005,0,0,0,0,0,0], #delta receptor
-                         [-10,0,0,0,0,0,0], #notch receptor
-                         [0,0,0,0,0,0,0], #ligand_delta
-                         [0,0,0,0,0,0,0], #ligand_notch
-                        ])
+trans_mat = numpy.array([[0,-1,0,0,0,10], #notch
+                         [0,0,0,0,1,0], #Delta
+                         [0.0005,0,0,0,0,0], #delta receptor
+                         [-10,0,0,0,0,0], #notch receptor
+                         [0,0,0,0,0,0], #ligand_delta
+                         [0,0,0,0,0,0] #ligand_notch
+                         ])
 
 init_pop = generate_pop([(2500, base1)])
 grid = HexGrid(50, 50, 1)
@@ -29,10 +28,8 @@ grid = HexGrid(50, 50, 1)
 def color_fun(row):
     if row[0]==1:
         return 1
-    elif row[4]==1:
-        return 0.75
     elif row[3]==1:
-        return 0.2
+        return 0.75
     else:
         return 0.
 
@@ -40,9 +37,9 @@ def color_fun(row):
 
 def run():
     x = Stops2(trans_mat, init_pop, grid.adj_mat, bound, secretion, reception, receptors, secr_amount=6, leak=0, max_con=6, max_dist=1.5, opencl=False)
-    for i in range(500):
+    for i in range(10000):
         x.step()
-        if i%10 == 0:
+        if i%100 == 0:
             print i
             draw_hex_grid("pics/f%04d.png"%i, x.pop, grid, color_fun)
 
