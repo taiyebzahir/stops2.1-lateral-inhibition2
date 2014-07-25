d6f6e3b0c1b3a6573d93634b8aed37351267b30a
taiyeb zahir <taiyebzahir13@gmail.com>
2014-07-25 15:10:42 +0200

    Update example_perfect_pattern.py

--- example_perfect_pattern.py
+++ example_perfect_pattern.py
@@ -6,27 +6,27 @@ import numpy
 from utils import generate_pop, HexGrid, draw_hex_grid
 from stops_ import Stops2
 
-secretion = numpy.array([4, 5])
-reception = numpy.array([2, 3])
-receptors = numpy.array([-1, -1])
+secretion = numpy.array([0, 5])
+reception = numpy.array([3, 4])
+receptors = numpy.array([-1,-1])
 bound=numpy.array([1,1,1,1,1,1])
 
-base1=numpy.array([0,1,0,0,0,0])
+base1=numpy.array([0,0,1,0,0,0])
 
 
-trans_mat = numpy.array([[0,-1,0,0,0,10], #notch
-                         [0,0,0,0,1,0], #Delta
-                         [0.0005,0,0,0,0,0], #delta receptor
-                         [-10,0,0,0,0,0], #notch receptor
-                         [0,0,0,0,0,0], #ligand_delta
-                         [0,0,0,0,0,0] #ligand_notch
+trans_mat = numpy.array([[0,0,0,0,0,0], #Delta_ligand
+                         [20,0,2,0,0,0], #dummy
+	[0,0.0005,0,0,1,0], #Delta
+                         [0,-1,-2,0,0,1], #notch receptor
+                         [0,0,1,0,0,0],#basal
+                         [0,0,0,0,0,0] #sequential activation
                          ])
 
 init_pop = generate_pop([(2500, base1)])
 grid = HexGrid(50, 50, 1)
 
 def color_fun(row):
-    if row[0]==1:
+    if row[2]==1:
         return 1
     elif row[3]==1:
         return 0.75
